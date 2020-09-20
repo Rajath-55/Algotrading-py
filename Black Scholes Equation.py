0dd82d6215e37688b367a49941c3bf2c4890ca06
Aditya Bhat <aditya.bhat2001@gmail.com>
2020-09-20 18:47:51 +0530

    added BSE

--- Black Scholes Equation.py
+++ Black Scholes Equation.py
@@ -1,9 +1,13 @@
 import numpy as np
-from scipy.stats import norm
+import scipy.stats as si
+from sympy.stats import Normal, cdf
 
 #function returning the call price option
 
-def BSE(S, K, stddev, r, t):
-    d1 = (np.log(S/K) + (r + (stddev**2/2) * t)/stddev * np.sqrt(t)
-    d2 = d1 - (stddev * np.sqrt(t))
-    return (S * norm.cdf(d1()) - K * np.exp(-r * t) * norm.cdf(d2()))
+def BSE(S, K, stdev, r, t):
+    d1 = (np.log(S / K) + (r + (stdev ** 2 / 2)) * t) / (stdev * np.sqrt(t))
+    d2 = (np.log(S / K) + (r - (stdev ** 2 / 2)) * t) / (stdev * np.sqrt(t))
+    call = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-r * t) * si.norm.cdf(d2, 0.0, 1.0))
+    return call
+
+print(BSE(50, 100, 0.25, 0.05, 1))
